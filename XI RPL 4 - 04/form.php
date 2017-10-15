c050ae085b02e06114eb5cdb941af57dd8eaa4ef
alyatsabitah <30452658+alyatsabitah@users.noreply.github.com>
2017-10-15 22:02:15 +0700

    Add files via upload

---
+++ XI RPL 4 - 04/form.php
@@ -0,0 +1,31 @@
+<html>
+<html lang="id" class="no-js not-logged-in ">
+	<head>
+	  <link rel="mask-icon" href="//instagramstatic-a.akamaihd.net/h1/images/ico/favicon.svg/9d8680ab8a3c.svg" color="#262626">
+	  <link rel="shortcut icon" type="image/x-icon" href="//instagramstatic-a.akamaihd.net/h1/images/ico/favicon.ico/dfa85bb1fd63.ico">
+	  <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'>
+	  <style class="cp-pen-styles">
+	    @import url(https://fonts.googleapis.com/css?family=Open+Sans);
+	    body{background-color:#FAFAFA;color:#3A4750;font-family:Open Sans}.login-box
+	        {margin-top:100px;text-align:center;width:430px;height:620px;
+	        background-color:#fff;padding:60px}.logo
+	        {height:70px;margin-bottom:30px}.form-control{margin-bottom:15px}.btn
+	        {width:100%;font-size:16px}
+	  </style>
+	<title>Instagram</title>
+	<body>
+		<div class="well login-box center-block"> 
+	    <img class="logo" src="http://www.dallasarboretum.org/assets/img/instagram-word.png"></img>
+	    <h4 class="_mb54c">Sign up to see photos and videos from your friends.</h4>
+	<form action="hasil.php" method="POST">
+		<hr>
+		<input type="email" class="form-control" placeholder="Email" name="email" required/> 
+	    <input type="text" class="form-control" placeholder="Full name" name="fullname" required/> 
+	    <input type="text" class="form-control" placeholder="Username" name="username" required/> 
+	    <input type="password" class="form-control" placeholder="Password" name="password" required/> 
+	    <button class="btn btn-primary" type="submit" id="submit">Sign Up</button>
+	    <hr>
+	    <p class="_fd2m9">By signing up, you agree to our<br/>Terms &amp; Privacy Policy</a></p> 
+	</form>
+	</body>
+</html>
